7dc5b1e744bff39c4967c4ba4034b8a9760e858b
Viraj Mathur <70805976+Viraj-Mathur@users.noreply.github.com>
2023-09-22 11:54:45 +0530

    Update circ_conv.m

--- circ_conv.m
+++ circ_conv.m
@@ -1,1 +1,26 @@
+clc
+clear all
+close all
+x = [1 0 2.5 1.5];
+h = [1 1 0.5 2];
+x1 = length(x);
+h1 = length(h);
 
+N = max(x1,h1);
+x_ext = [x, zeros(1,N-x1)];
+h_ext = [h, zeros(1,N-h1)];
+
+y = zeros(1,N);
+for i=1:N
+    for j=1:N
+        y(i) = y(i) + x_ext(i)*h_ext(mod((i-j),N)+1)
+    end
+end
+
+figure
+stem(y);
+grid on
+title("Circular Convolution");
+xlabel("sample (n)")
+ylabel("Amplitude")
+legend()
